faca941f2d2c769ec22294c08193808ad6dfe049
brianh618 <bherrera10@mail.csuchico.edu>
2018-02-01 11:49:40 -0800

    describe what is new in your own words

--- madlibs.cpp
+++ madlibs.cpp
@@ -4,6 +4,25 @@
 
 int main()
 {
+  std::string name;
+  int number;
+  char punctuation;
+  std::string verb;
+  
+  std::cout<<"Name \n";
+  std::cin>> name;
+  
+  std::cout<<"Pucnctuation \n";
+  std::cin>> punctuation;
+  
+  std::cout<<"Enter a number \n";
+  std::cin>> number;
+  
+  std::cout<<"Enter a intense destructive verb \n";
+  std::cin>> verb;
+
+std::cout<<"\n "<<name<< punctuation<< "\n Stop eating hotdogs! You've already had "<<number <<".\n Give me that hotdog. Otherwise you'll "<<verb <<".\n\n"; 
+
 
   return 0;
 }
